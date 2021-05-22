834910084073f3876f2dd896b76837cd9ed0fdea
S Nitesh Singh <niteshseram@gmail.com>
2021-05-22 17:29:33 +0530

    adding signup form

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -3,9 +3,30 @@ import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const Signin = () => {
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-md-3 col-12 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group my-2">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <div class="d-grid gap-2 my-3">
+              <button className="btn btn-success">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
   return (
     <Base title="Signin Page" description="A page for user to signin!">
-      <h1>SignIn Works</h1>
+      {signInForm()}
     </Base>
   );
 };
