ba8d063aac300804364aff93e05c9f8b3efaa03e
S Nitesh Singh <niteshseram@gmail.com>
2021-05-22 16:19:09 +0530

    adding Nav Bar

---
+++ src/core/NavBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-dark">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Cart
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Admin Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Signup
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Sign In
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Signout
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default withRouter(NavBar);
