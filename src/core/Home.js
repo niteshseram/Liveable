a1610594f934be053a0e51f8921e65c5c580fe7a
S Nitesh Singh <niteshseram@gmail.com>
2021-05-22 16:02:47 +0530

    adding some base component

---
+++ src/core/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles.css";
+import Base from "./Base";
+import { API } from "../backend";
+
+export default function Home() {
+  return (
+    <Base title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </Base>
+  );
+}
