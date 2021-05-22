31f0417952d8d8d58f1b2b54cf003dda36dc75e1
S Nitesh Singh <niteshseram@gmail.com>
2021-05-22 17:29:18 +0530

    adding signyp form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -3,9 +3,35 @@ import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const signUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-md-3 col-12 text-left">
+          <form>
+            <div className="form-group ">
+              <label className="text-light">Name</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group my-2">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group my-2">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <div class="d-grid gap-2 my-3">
+              <button className="btn btn-success">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Base title="Signup Page" description="A page for user to signup!">
-      <h1>SignUp Works</h1>
+      {signUpForm()}
     </Base>
   );
 };
