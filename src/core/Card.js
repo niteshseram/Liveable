9ff7a13e0072d099d095b113294ae1fec4b30e94
S Nitesh Singh <niteshseram@gmail.com>
2021-05-24 11:56:17 +0530

    adding add to cart funtionality

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router";
+import { addItemToCart } from "./helper/cartHelper";
 import ImageHelper from "./helper/ImageHelper";
 
 const Card = ({ product, addToCart = true, removeFromCart = false }) => {
+  const [redirect, setRedirect] = useState(false);
+
   const productTitle = product ? product.name : "A default title";
   const productDescription = product
     ? product.description
     : "A default description";
   const productPrice = product ? product.price : 1000;
 
+  const addProductToCart = () => {
+    addItemToCart(product, () => setRedirect(true));
+  };
+
+  const getARedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
   const showAddToCart = (addToCart) => {
     return (
       addToCart && (
         <button
-          onClick={() => {}}
+          onClick={addProductToCart}
           className="btn btn-block btn-outline-success mt-2 mb-2"
         >
           Add to Cart
@@ -38,6 +52,7 @@ const Card = ({ product, addToCart = true, removeFromCart = false }) => {
     <div className="card text-white bg-dark border border-info text-center">
       <div className="card-header lead">{productTitle}</div>
       <div className="card-body">
+        {getARedirect(redirect)}
         <ImageHelper product={product} />
         <p className="lead bg-success font-weight-normal text-wrap">
           {productDescription}
---
+++ src/core/Cart.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import "../styles.css";
+import Base from "./Base";
+import Card from "./Card";
+import { loadCart } from "./helper/cartHelper";
+
+const Cart = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(loadCart());
+  }, []);
+
+  const loadAllProduct = () => {
+    return (
+      <div>
+        <h2>This section is load products</h2>
+        {products.map((product, index) => {
+          return (
+            <Card
+              key={index}
+              product={product}
+              addToCart={false}
+              removeFromCart={true}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+
+  const loadCheckout = () => {
+    return (
+      <div>
+        <h2>This section is for checkout</h2>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Cart Page" description="Ready to checkout">
+      <div className="row">
+        <div className="col-6">{loadAllProduct()}</div>
+        <div className="col-6">{loadCheckout()}</div>
+      </div>
+    </Base>
+  );
+};
+
+export default Cart;
